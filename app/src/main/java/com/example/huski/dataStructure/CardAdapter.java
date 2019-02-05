bd2a3cf70d9562f1638ace8e87d1b0c29fd8a246
TitouanLarnicol <larnicol.titouan@orange.fr>
2019-02-05 22:47:17 +0100

    rename funct working

--- app/src/main/java/com/example/huski/dataStructure/CardAdapter.java
+++ app/src/main/java/com/example/huski/dataStructure/CardAdapter.java
@@ -1,6 +1,8 @@
 package com.example.huski.dataStructure;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -9,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,9 +25,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardAdapter extends ArrayAdapter<cardStruct> {
+    //Variable definition
     Activity activity;
     FindFragment findFragment;
-    public ImageButton localiseBtn;
+    AlertDialog.Builder dialog;
+    ImageButton localiseBtn,deleteBtn;
+    TextView cardName;
+
     public CardAdapter(Activity activity, ArrayList<cardStruct> cards,FindFragment findFragment){
         super(activity,0,cards);
         this.activity = activity;
@@ -32,18 +39,18 @@ public class CardAdapter extends ArrayAdapter<cardStruct> {
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(final int position,View convertView, ViewGroup parent) {
         // Get the data item for this position
         final cardStruct card = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view, parent, false);
         }
-        // Lookup view for data population
-        final TextView cardName = (TextView) convertView.findViewById(R.id.cardName);
-        ImageButton deleteBtn = (ImageButton) convertView.findViewById(R.id.deleteButton);
+        // Link to XML
+        cardName = convertView.findViewById(R.id.cardName);
+        deleteBtn = convertView.findViewById(R.id.deleteButton);
         localiseBtn =  convertView.findViewById(R.id.localiseButton);
-        // Populate the data into the template view using the data object
+        // set cardName
         cardName.setText(card.getName());
 
         deleteBtn.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +70,36 @@ public class CardAdapter extends ArrayAdapter<cardStruct> {
             }
         });
 
+        //Alert to edit text
 
+        cardName.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                final View view = LayoutInflater.from(getContext()).inflate(R.layout.popup, null);
+                final EditText renameInput = (EditText) view.findViewById(R.id.rename);
+                dialog = new AlertDialog.Builder(getContext());
+                dialog.create();
+                dialog.setTitle("Edit the name");
+                dialog.setPositiveButton("Rename", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        card.setName(renameInput.getText().toString());
+                        Toast.makeText(activity, "change"+card.getName(), Toast.LENGTH_SHORT).show();
+                        cardName.setText(card.getName());
+                        ListFragment.adapter.notifyDataSetChanged();
+                    }
+                });
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.setView(view);
+                dialog.show();
+                return false;
+            }
+        });
 
         // Return the completed view to render on screen
         return convertView;
