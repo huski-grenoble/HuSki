818e06fef4a15c121a58d38ad8340123d3d7c28d
tozenda <thom.ozenda@gmail.com>
2019-02-10 18:53:08 +0100

    NEW : 1st version with QrCode reader working

--- app/src/main/java/com/example/huski/AddFragment.java
+++ app/src/main/java/com/example/huski/AddFragment.java
@@ -2,19 +2,71 @@ package com.example.huski;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
 
 
-public class AddFragment extends Fragment {
+public class AddFragment extends Fragment implements BarcodeReader.BarcodeReaderListener {
+
+    private BarcodeReader barcodeReader;
 
     public static AddFragment newInstance() {
         return (new AddFragment());
     }
 
-    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View v = inflater.inflate(R.layout.fragment_add, container, false);
+
+        barcodeReader = (BarcodeReader) getChildFragmentManager().findFragmentById(R.id.barcode_scanner);
+        barcodeReader.setListener(this);
+        return v;
+    }
+
+
+    @Override
+    public void onScanned(final Barcode barcode) {
+        barcodeReader.playBeep();
+
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getActivity(), "Barcode: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+
     }
 }
